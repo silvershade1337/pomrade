5639297d374cbf9eed31fdb988b60d964edf7311
Aneesh <aneeshyr2@gmail.com>
2023-10-10 17:55:34 +0530

    added responsive abstract image to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:pomrade/screens/login.dart';
-import 'dart:io';
 
+// SCREENS
+import 'screens/login.dart';
 import 'screens/home.dart';
 
 void main() {
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,65 +10,76 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    Size availablesize = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 600),
-            child: Column(
-              // scrollDirection: Axis.vertical,
-              children: [
-                Image.asset("assets/pomrade.png", width: 200,),
-                // FittedBox(
-                //   fit: BoxFit.scaleDown, // fixes fittedbox not working in listview
-                //   child: Padding(
-                //   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                //   child: Text("Pomrade", style: TextStyle(fontSize: 130, fontWeight: FontWeight.bold),),
-                // )),
-                
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: 450),
-                      child: Column(
-                        children: [
-                          FittedBox(child: Text("Sign into Pomrade", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600, color: Colors.deepPurple[200]))),
-                          TextField(
-                            decoration: InputDecoration(
-                              label: Text("Username"),
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(200)),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 20)  
+        child: Row(
+          children: [
+            if (availablesize.width > 800) ...[Image.asset("assets/abstractbg.jpg"),],
+            
+            Expanded(
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  child: Column(
+                    // scrollDirection: Axis.vertical,
+                    children: [
+                      Image.asset("assets/pomrade.png", width: 200,),
+                      // FittedBox(
+                      //   fit: BoxFit.scaleDown, // fixes fittedbox not working in listview
+                      //   child: Padding(
+                      //   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                      //   child: Text("Pomrade", style: TextStyle(fontSize: 130, fontWeight: FontWeight.bold),),
+                      // )),
+                      
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Form(
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(maxWidth: 450),
+                            child: Column(
+                              children: [
+                                FittedBox(child: Text("Sign into Pomrade", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600, color: Colors.deepPurple[200]))),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    label: Text("Username"),
+                                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(200)),
+                                    contentPadding: EdgeInsets.symmetric(horizontal: 20)  
+                                  ),
+                                  
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    label: Text("Password"),
+                                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(200)),
+                                    contentPadding: EdgeInsets.symmetric(horizontal: 20)  
+                                  ),
+                                  obscureText: true,
+                                  
+                                ),
+                                FittedBox(
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                    
+                                    }, 
+                                    child: Text("SIGN IN"),
+                                    style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple[200], foregroundColor: Colors.white, padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20)), 
+                                  ),
+                                )
+                              ].map((e) => Padding(padding: EdgeInsets.only(top: 20, left: 10, right: 10), child: e,)).toList(),
                             ),
-                            
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                              label: Text("Password"),
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(200)),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 20)  
-                            ),
-                            obscureText: true,
-                            
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                            
-                            }, 
-                            child: Text("SIGN IN"),
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple[200], foregroundColor: Colors.white, padding: EdgeInsets.symmetric(horizontal: 100, vertical: 25)), 
                           )
-                        ].map((e) => Padding(padding: EdgeInsets.all(10), child: e,)).toList(),
+                        ),
                       ),
-                    )
+                      TextButton(onPressed: () {
+                        
+                      }, child: Text("Don't have an account?", style: TextStyle(color: Colors.white),))
+                    ],
                   ),
                 ),
-                TextButton(onPressed: () {
-                  
-                }, child: Text("Don't have an account?", style: TextStyle(color: Colors.white),))
-              ],
+              ),
             ),
-          ),
+          ],
         )
       ),
     );
